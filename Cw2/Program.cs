8f877383de9e075a7f8f2aa4b3755a84b07e718a
Urszula Tarasewicz <s18309@pjwstk.edu.pl>
2020-03-13 19:13:03 +0100

    W sumie to całkiem działające zadanie

---
+++ Cw2/Program.cs
@@ -0,0 +1,121 @@
+﻿using cw2;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+
+namespace Cw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] argumenty = new string[3];
+
+
+            if (args.Length >= 1)
+                argumenty[0] = args[0];
+            else
+                argumenty[0] = "data.csv";
+
+            if (args.Length >= 2)
+                argumenty[1] = args[1];
+            else
+                argumenty[1] = "żesult.xml";
+
+
+            if (args.Length >= 3)
+                argumenty[2] = args[2];
+            else
+                argumenty[2] = "xml";
+
+            for (int i = 0; i < argumenty.Length; i++)
+                Console.WriteLine(argumenty[i]);
+
+
+            var log = File.Create("łog.txt");
+
+            using (StreamWriter writer = new StreamWriter(log)) //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            {
+
+                try
+                {
+
+                    var setStudentow = new System.Collections.Generic.HashSet<Student>(new OwnComparator());
+
+
+                    using (var stream = new StreamReader(File.OpenRead(Path.GetFullPath(argumenty[0]))))
+                    {
+                        string line = null;
+                        var regex = new Regex("$\\s+");
+
+                        while ((line = stream.ReadLine()) != null)
+                        {
+
+                            bool wpisywac = true;
+
+                            string[] student = line.Split(',');
+
+
+                            for (int i = 0; i < student.Length; i++)
+                            {
+                                var matches = regex.Matches(student[i]);
+                                if(matches.Count >= 1)
+                                {
+                                    writer.WriteLine(line);
+                                    wpisywac = false;
+                                    break;
+                                }
+                            }
+
+                            if (student.Length == 9 && wpisywac == true)
+                            {
+                                var st = new Student
+                                {
+                                    index = student[4],
+                                    fName = student[0],
+                                    lName = student[1],
+                                    birthdate = student[5],
+                                    email = student[6],
+                                    mothersName = student[7],
+                                    fathersName = student[8],
+                                    studiesName = student[2],
+                                    studiesMode = student[3]
+
+
+                                };
+                             
+
+                                if (!setStudentow.Add(st))
+                                {                                  
+                                    writer.WriteLine(line);
+                                }
+
+
+                            }
+
+                            FileStream XMLwriter = new FileStream(argumenty[1], FileMode.Create);
+                            XmlSerializer serializer = new XmlSerializer(typeof(HashSet<Student>), new XmlRootAttribute("uczelnia"));
+                            serializer.Serialize(XMLwriter, setStudentow);
+                            XMLwriter.Close();
+                            XMLwriter.Dispose();
+                        }
+                    }
+
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine("Podana ścieżka jest niepoprawna");
+                    writer.WriteLine("Podana ścieżka jest niepoprawna");
+                }
+                catch (FileNotFoundException e)
+                {
+                    Console.WriteLine("Plik nazwa nie istnieje");
+                    writer.WriteLine("Plik nazwa nie istnieje");
+                }
+            }
+
+        }
+    }
+}
